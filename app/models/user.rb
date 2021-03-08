ebf3d72fafa3a7e3567b5548fb2b9013df5d972c
ybadmus <yusif.badmus@gmail.com>
2021-03-08 22:19:08 +0000

    adding remember me feature to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, length: { in: 20...150 }
+  validates :username, presence: true, length: { in: 20...150 }
+  validates :password, presence: true, length: { in: 4...50 }
+  password_confirmation, presence: true, length: { in: 4...50 }
+  has_secure_password
 end
